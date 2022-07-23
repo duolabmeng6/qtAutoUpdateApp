af41663d1c93569929d25dc09c7a40a6cdf2d61e
duolabmeng <1715109585@qq.com>
2022-07-24 01:25:10 +0800

    加入自动检测

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
 
-
 def 查看系统所有环境变量():
     from icecream import ic
     # 打印系统所有的环境变量
@@ -9,17 +8,18 @@ def 查看系统所有环境变量():
         name = item
         value = os.environ[item]
         ic(name, value)
-查看系统所有环境变量()
-
+# 查看系统所有环境变量()
 tagName = os.environ.get('GITHUB_REF_NAME')
 print("tagName:", tagName)
 # 获取运行目录
-print("run dir:", os.getcwd())
+# print("run dir:", os.getcwd())
 # 获取文件目录
 文件目录 = os.path.dirname(__file__)
 print("file dir:", os.path.dirname(__file__))
+versionFilePath = os.path.join(文件目录, "version.txt")
+print("edit file {versionFilePath} output: version = {tagName}")
 # 覆盖写出文件 version.py 中
-with open(f'{文件目录}/version.py', 'w') as f:
+with open(versionFilePath, 'w') as f:
     f.write(f'version = "{tagName}"')
 
 exit()
