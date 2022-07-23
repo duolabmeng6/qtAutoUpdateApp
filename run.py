5059788d54d750f8fa04c0614723392b38ceabdb
duolabmeng <1715109585@qq.com>
2022-07-24 01:16:08 +0800

    不信邪...

--- run.py
+++ run.py
@@ -13,8 +13,13 @@ def 查看系统所有环境变量():
 
 tagName = os.environ.get('GITHUB_REF_NAME')
 print("tagName:", tagName)
+# 获取运行目录
+print("run dir:", os.getcwd())
+# 获取文件目录
+文件目录 = os.path.dirname(__file__)
+print("file dir:", os.path.dirname(__file__))
 # 覆盖写出文件 version.py 中
-with open('version.py', 'w') as f:
+with open(f'{文件目录}/version.py', 'w') as f:
     f.write(f'version = "{tagName}"')
 
 exit()
