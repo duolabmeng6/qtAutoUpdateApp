4478f261a06b314f627aab66a05b41d3d4672f13
duolabmeng <1715109585@qq.com>
2022-07-24 05:11:46 +0800

    加入教程

--- run_output_version.py
+++ run_output_version.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 import os
 
-def 查看系统所有环境变量():
+
+def view_all_environment_variables():
     from icecream import ic
-    # 打印系统所有的环境变量
     for item in os.environ:
         name = item
         value = os.environ[item]
         ic(name, value)
-# 查看系统所有环境变量()
+
+
+# view_all_environment_variables()
+
 tagName = os.environ.get('GITHUB_REF_NAME')
 print("tagName:", tagName)
-# 获取运行目录
-# print("run dir:", os.getcwd())
-# 获取文件目录
-文件目录 = os.path.dirname(__file__)
-print("file dir:", os.path.dirname(__file__))
-versionFilePath = os.path.join(文件目录, "version.txt")
-print("edit file {versionFilePath} output: version = {tagName}")
-# 覆盖写出文件 version.py 中
+fileDir = os.path.dirname(__file__)
+print("file dir:", fileDir)
+versionFilePath = os.path.join(fileDir, "version.py")
+print(f"edit file {versionFilePath} output: version = {tagName}")
 with open(versionFilePath, 'w') as f:
     f.write(f'version = "{tagName}"')
 
